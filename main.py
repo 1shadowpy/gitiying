da774c255f1184815a7eeb2f121726b4469d33b0
1shadowpy <132641739+1shadowpy@users.noreply.github.com>
2023-06-01 15:09:44 +0500

    first commit

--- main.py
+++ main.py
@@ -1,41 +1,38 @@
-# gittler = {}
-# putin =  {}
-# ranaldoooooooo = {}
-# gittler['shahri'] =  'germaniya'
-# gittler['fani'] = 'qilinch vozlik'
-# gittler['olgani haqida'] = 'urishda olgan'
-# putin['shahri'] = 'russiya'
-# putin['fani'] = 'prisinent'
-# putin['olgani haqida'] = 'hech narsa yoq'
-# ranaldoooooooo['shahri']=  'portgaliya'
-# ranaldoooooooo['fani'] = 'futbolist'
-# ranaldoooooooo['olgani haqida'] = 'hech narsa yoq'
-# print(f"gittler tugilgan shahri bu {gittler['shahri']} ,\n gittlerni yahshi korgan fani bu {gittler['fani']} ,\n gitlerni olgani haqida malumot {gittler['olgani haqida']}")
-# print(f"putinni tugilgan shahri bu  {putin['shahri']} , \n putinni  yahshi korgan fani bu {putin['fani']} , \n putini olgani haqida malumot {putin['olgani haqida']}")
-# print(f"ranaldooooooooni tugilgan shahri bu  {ranaldoooooooo['shahri']} , \n ranaldooooooooni yahshi korgan fani bu {ranaldoooooooo['fani']} , \n ranaldooooooooni olgani haqida malumot {ranaldoooooooo['olgani haqida']}")
-# print("gittlerning chiroyli asarlari :\n franchi in my heands \n russiya in my heands \n head giiiiting")
-# print("putini chirroyli asarlari : \n dengi \n givver \n shkibidi dop dop dop yes yes yes")
-# print("ranaldooooooooni chiroyli asarlari : \n real madrit \n city von \n barsa loase")
+# print("yahshi korgan kitobingizni kiriting")
+# savol = "Istalgan kiriting kiriting "
+# savol += "(dasturni to'xtatish uchun 'exit' deb yozing): "
+# kitob  = ''
+# while kitob != 'stop':
+#     kitob  = input(savol)
+#     if kitob != 'stop':
+#         print("sizga sovga 'OTGAN KUNLAR' deggan kitob")
 
-# ukamning = {'xonazimbek':'tarzan,maugli,avatar'}
-# qoshnimning = {'shaxzod':'farsaj,12kvartal,tumsoq qasosi'}
-# dostim = {'farxod':'qassoskorlar,hursand-poyezd,qassos'}
-#
-# print(f"ukamning yahshikorgan filmi bu: \n {ukamning['xonazimbek']}")
-# print(f"qoshnimning yahshikorgan filmi bu: \n {qoshnimning['shaxzod']}")
-# print(f"dostiminhg yahshikorgan filmi bu: \n {dostim['farxod']}")
-#
-# germaniya = {'shahari':'kop','aholi':'999999999999999','pull miqdori':'filslip'}
-# serbiya = {'shahari':'13','aholi':'456677777777','pull miqdori':'driltil'}
-# finlandiya = {'shahari':'7','aholi':'34567891234','pull miqdori':'punt'}
-# print(germaniya)
-# print(serbiya)
-# print(finlandiya)
-# germaniya = {'shahari':'kop','aholi':'999999999999999','pull miqdori':'filslip'}
-# serbiya = {'shahari':'13','aholi':'456677777777','pull miqdori':'driltil'}
-# finlandiya = {'shahari':'7','aholi':'34567891234','pull miqdori':'punt'}
-# dilgerlid = {'shahari':'90','aholi':'454545454','pull miqdori':'kilnot'}
-# a = input('davlatni kiriting:')
+# dilver = ("yoshingizni  kiriting")
+# bir,olti = 2000,2500
+# yetti,onikki = 3000,3500
+# onuch,onyetti = 10000,12500
+# onsakiz,oltmishtort = 15000,15500
+# oltmishbesh : 0
+# a = True
+# while a !='stop':
+#     a = input(dilver)
+#     if a != 'stop':
+#         print(a)
+# else:
+#     print('siz yoshingizni qayta kiriting')
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
